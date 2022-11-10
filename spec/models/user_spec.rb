fc3605b9ebc262434e474991e50805485aa5a905
Christoph Matthies <chrisma@users.noreply.github.com>
2022-11-10 16:15:11 +0100

    Test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  let(:user) { FactoryBot.build :user }
+
+  it "can be created using a factory" do
+    expect(user).to be_valid
+  end
+
+  it "sets an email" do
+    expect(user.email).not_to be_blank
+  end
 end
